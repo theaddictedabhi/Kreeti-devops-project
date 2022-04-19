57c589455cfe112a415a5acc5d852c44626e5770
abhimanyu <manyu.abhigoswami@gmail.com>
2022-04-20 03:37:20 +0530

    my final commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Abhimanyu mera nam');
+  res.send('Abhimanyu mera naam');
 });
 app.get('/home', (req, res) => {
   res.send('home is working!');
